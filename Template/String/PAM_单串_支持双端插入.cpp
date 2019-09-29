b42121a7d3f480b65a9fef091cbd9878d596ece5
jslijin <42512810+jslijin@users.noreply.github.com>
2019-09-29 20:22:24 +0800

    1212

--- Template/String/PAM_单串_支持双端插入.cpp
+++ Template/String/PAM_单串_支持双端插入.cpp
@@ -1,34 +1,34 @@
-const int M=26;
-struct PAM{
-	int ss[2*N],*s,len[N],next[N][M],fail[N],cnt[N],dep[N],id[N],no[N],last[2],n[2],p,cur,now,t; ll sum_cnt,ans;
-	inline int new_node(int _l) { mem(next[p],0); cnt[p]=dep[p]=0,len[p]=_l; return p++; }
-	inline void Init() { s=ss+N; new_node(p=0),new_node(s[0]=s[1]=-1),fail[last[0]=last[1]=n[1]=0]=n[0]=1; /*...*/ sum_cnt=0; }
-	inline int get_fail(int x,int op) { int t=op*2-1; for (; s[n[op]-t*(len[x]+1)]!=s[n[op]]; x=fail[x]); return x; }
-	inline void I(int c,int op) { 
-		c-='a',t=op*2-1,s[n[op]+=t]=c,s[n[op]+t]=-1,cur=get_fail(last[op],op);
+const int M = 26;
+struct PAM {
+	int ss[2 * N], *s, len[N], next[N][M], fail[N], cnt[N], dep[N], id[N], no[N], last[2], n[2], p, cur, now, t; ll sum_cnt, ans;
+	inline int new_node(int _l) { mem(next[p], 0); cnt[p] = dep[p] = 0, len[p] = _l; return p++; }
+	inline void Init() { s = ss + N; new_node(p = 0), new_node(s[0] = s[1] = -1), fail[last[0] = last[1] = n[1] = 0] = n[0] = 1; /*...*/ sum_cnt = 0; }
+	inline int get_fail(int x, int op) { int t = op * 2 - 1; for (; s[n[op] - t*(len[x] + 1)] != s[n[op]]; x = fail[x]); return x; }
+	inline void I(int c, int op) {
+		c -= 'a', t = op * 2 - 1, s[n[op] += t] = c, s[n[op] + t] = -1, cur = get_fail(last[op], op);
 		if (!next[cur][c]) {
-			now=new_node(len[cur]+2);
-			fail[now]=next[get_fail(fail[cur],op)][c];
-			next[cur][c]=now;
-			dep[now]=dep[fail[now]]+1; //...
+			now = new_node(len[cur] + 2);
+			fail[now] = next[get_fail(fail[cur], op)][c];
+			next[cur][c] = now;
+			dep[now] = dep[fail[now]] + 1; //...
 		}
-		last[op]=next[cur][c],cnt[last[op]]++;
-		if (len[last[op]]==n[1]-n[0]+1) last[op^1]=last[op];
-		id[n[op]]=last[op]; no[last[op]]=n[op]+(len[last[op]]-1)*!op; //...
-		sum_cnt+=dep[last[op]];
+		last[op] = next[cur][c], cnt[last[op]]++;
+		if (len[last[op]] == n[1] - n[0] + 1) last[op ^ 1] = last[op];
+		id[n[op]] = last[op]; no[last[op]] = n[op] + (len[last[op]] - 1) * !op; //...
+		sum_cnt += dep[last[op]];
 	}
-	inline void Insert(char s[],int back=1,int op=0,int _n=0) { 
-		if (!_n) _n=strlen(s);  if (!op) rep(i,0,_n) I(s[i],back); else per(i,0,_n) I(s[i],back); 
+	inline void Insert(char s[], int back = 1, int op = 0, int _n = 0) {
+		if (!_n) _n = strlen(s);  if (!op) rep(i, 0, _n) I(s[i], back); else per(i, 0, _n) I(s[i], back);
 	}
-	inline void count() { per(i,0,p) cnt[fail[i]]+=cnt[i]; }
-	inline void Q(){ /*count();*/ }
+	inline void count() { per(i, 0, p) cnt[fail[i]] += cnt[i]; }
+	inline void Q() { /*count();*/ }
 };
 
 /*
 
-注： 1) 支持在线维护本质不同回文串个数 p-2 , 所有回文串个数 sum_cnt , 每个回文串出现的一次起点下标 ( 不保证最左最右 ) 
+注： 1) 支持在线维护本质不同回文串个数 p-2 , 所有回文串个数 sum_cnt , 每个回文串出现的一次起点下标 ( 不保证最左最右 )
 
-     2) 注意 id[x] 是不准确的
+2) 注意 id[x] 是不准确的
 
 */
 
--- Template/String/PAM_单串_支持撤销_单加log.cpp
+++ Template/String/PAM_单串_支持撤销_单加log.cpp
@@ -1,27 +1,27 @@
-const int M=26;
-struct PAM{
-    int s[N],len[N],next[N][M],fail[N],cnt[N],id[N],no[N],pre[N],qlink[N],dep[N],last,n,p,cur,now; ll ans;
-    inline int new_node(int _l) { mem(next[p],0); cnt[p]=dep[p]=0,len[p]=_l; qlink[p]=0; return p++; }
-    inline void Init() { new_node(p=0),new_node(s[0]=-1),fail[last=n=0]=1; /* ... */ }
-    inline bool ok(int x,int y,int d=0) { return s[n-len[x]-d]==s[n-len[y]]; }
-    inline int get_fail(int x) { for (; !ok(x,0,1); x=qlink[x]) if (ok(fail[x],0,1)) return fail[x]; return x; }
-    inline void I(int c) { 
-        c-='a',s[++n]=c,cur=get_fail(last);
-        if (!next[cur][c]) {
-            now=new_node(len[cur]+2);
-            fail[now]=next[get_fail(fail[cur])][c];
-            next[cur][c]=now; pre[now]=cur;
-            dep[now]=dep[fail[now]]+1; //...
-            if (len[now]>1) qlink[now]=ok(fail[now],fail[fail[now]]) ? qlink[fail[now]] : fail[fail[now]];
-        }
-        last=next[cur][c],cnt[last]++; id[n]=last,no[last]=n; //...
-    }
-    inline void D() { if (p<=1) return; if (!(--cnt[last])) next[pre[last]][s[n]]=0,--p; last=id[--n]; }
-    inline void Insert(char s[],int op=0,int _n=0) { 
-        if (!_n) _n=strlen(s);  if (!op) rep(i,0,_n) I(s[i]); else per(i,0,_n) I(s[i]); 
-    }
-    inline void count() { per(i,0,p) cnt[fail[i]]+=cnt[i]; }
-    inline void Q(){ /*count();*/ }
+const int M = 26;
+struct PAM {
+	int s[N], len[N], next[N][M], fail[N], cnt[N], id[N], no[N], pre[N], qlink[N], dep[N], last, n, p, cur, now; ll ans;
+	inline int new_node(int _l) { mem(next[p], 0); cnt[p] = dep[p] = 0, len[p] = _l; qlink[p] = 0; return p++; }
+	inline void Init() { new_node(p = 0), new_node(s[0] = -1), fail[last = n = 0] = 1; /* ... */ }
+	inline bool ok(int x, int y, int d = 0) { return s[n - len[x] - d] == s[n - len[y]]; }
+	inline int get_fail(int x) { for (; !ok(x, 0, 1); x = qlink[x]) if (ok(fail[x], 0, 1)) return fail[x]; return x; }
+	inline void I(int c) {
+		c -= 'a', s[++n] = c, cur = get_fail(last);
+		if (!next[cur][c]) {
+			now = new_node(len[cur] + 2);
+			fail[now] = next[get_fail(fail[cur])][c];
+			next[cur][c] = now; pre[now] = cur;
+			dep[now] = dep[fail[now]] + 1; //...
+			if (len[now] > 1) qlink[now] = ok(fail[now], fail[fail[now]]) ? qlink[fail[now]] : fail[fail[now]];
+		}
+		last = next[cur][c], cnt[last]++; id[n] = last, no[last] = n; //...
+	}
+	inline void D() { if (p <= 1) return; if (!(--cnt[last])) next[pre[last]][s[n]] = 0, --p; last = id[--n]; }
+	inline void Insert(char s[], int op = 0, int _n = 0) {
+		if (!_n) _n = strlen(s);  if (!op) rep(i, 0, _n) I(s[i]); else per(i, 0, _n) I(s[i]);
+	}
+	inline void count() { per(i, 0, p) cnt[fail[i]] += cnt[i]; }
+	inline void Q() { /*count();*/ }
 };
 
 /*
@@ -30,6 +30,6 @@ struct PAM{
 
 1) 此模板如果不使用回退操作，总复杂度仍为线性 ，单次插入不超过 log(n) ，回退操作总是常数
 
-2) 若有回退操作，总复杂度退化；使用可持久化线段树优化的 dLink 链接，单次插入可降为 log 字符集 
+2) 若有回退操作，总复杂度退化；使用可持久化线段树优化的 dLink 链接，单次插入可降为 log 字符集
 
 */
