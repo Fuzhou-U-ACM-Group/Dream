a6d5698484588bbe650a88887019778286fb5195
jslijin <42512810+jslijin@users.noreply.github.com>
2019-10-10 16:16:39 +0800

    1212

---
+++ Template/Others/java/Map.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class code {
+
+    public static void main(String[] args) {
+        
+        Scanner cin=new Scanner(System.in);
+        
+//        BigInteger a=cin.nextBigInteger();
+//        BigInteger b=cin.nextBigInteger();
+        
+        Integer a,b;
+        a=cin.nextInt();
+        b=cin.nextInt();
+        System.out.println(a+b);
+        
+        
+        List<String> mylist1 = new ArrayList<>();
+		List<String> mylist2 = new LinkedList<>();
+		List<String> mylist3 = new Vector<>();
+ 
+		Vector<String> vec = new Vector<>();
+ 
+		Queue<String> que = new LinkedList<>();
+ 
+		Stack<String> sta = new Stack<>();
+ 
+		Set<String> myset = new HashSet<>();
+		Set<String> myset2 = new TreeSet<>();
+ 
+		Map<String, Integer> mymap = new HashMap<>();
+		Map<String, Integer> mymap2 = new TreeMap<>();
+                
+    }
+    
+}
