11e15e0d39323d6e12bad72420c9b5fe499c4263
jslijin <42512810+jslijin@users.noreply.github.com>
2019-10-17 19:05:55 +0800

    Create FWT_染色多项式.cpp

---
+++ Template/Math/FWT_染色多项式.cpp
@@ -0,0 +1,138 @@
+template<class T>
+struct Poly{
+	static const int N = 30, P = 1e9+7;
+	T a1[N], b1[N], c[N];
+	T add(T a, T b) {a = (a + b) % P; return a < 0 ? a + P : a;}
+	T mul(T a, T b) {a = 1ll * a * b % P; return a < 0 ? a + P : a;}
+	T kpow(T a, T b) {T r=1;for(;b;b>>=1,a=mul(a,a)) {if(b&1)r=mul(r,a);}return r;}
+	void calc(int n, T *a, T *b) {
+		fill_n(c, n+1, 0);
+		rep(i, 0, n+1)	rep(j, 0, 2) c[i+j] = add(c[i+j], mul(a[i], b[j]));
+		memcpy(a, c, sizeof(a[0]) * (n+1));
+	} 
+	void solve(int n, T *x, T *y, T *a){ // a[0]*x^0 ... a[n]*x^n
+		fill_n(a, n+1, 0);
+		rep(i, 0, n+1) {
+			fill_n(a1, n+1, 0); a1[0] = 1;
+			rep(j, 0, n+1) if (j != i) a1[0] = mul(a1[0], x[i] - x[j]);
+			a1[0] = mul(y[i], kpow(a1[0], P - 2));
+			rep(j, 0, n+1) if (j != i) {
+				b1[0] = -x[j]; b1[1] = 1;
+				calc(n, a1, b1);
+			}
+			rep(j, 0, n+1) a[j] = add(a[j], a1[j]);
+		}
+	}
+};
+
+const int P = 1e9 + 7, M = 15;
+inline int mul(int x, int y) { return (ll)x*y%P; }
+inline int add(int x, int y) { return (x += y) >= P ? x - P : x; }
+
+template <int L>
+struct Num {
+	array<int, L> a;
+	inline int& operator [] (int x) { return a[x]; }
+	inline int operator [] (int x) const { return a[x]; }
+	inline void clear() { a.fill(0); }
+	inline void operator += (const Num &b) {
+		rep(i, 0, L) a[i] = add(a[i], b[i]);
+	}
+	inline void operator -= (const Num &b) {
+		rep(i, 0, L) a[i] = add(a[i], P - b[i]);
+	}
+	inline friend Num operator * (const Num &a, const Num &b) {
+		Num<L> c; c.clear();
+		rep(i, 0, L) if (a[i])
+			for (int j = 0; i + j < L; ++j) if (b[j])
+				c[i + j] = add(c[i + j], mul(a[i], b[j]));
+		return c;
+	}
+};
+
+template <class V>
+struct Calculator {
+	V aa[1 << M], bb[1 << M];
+	void fwt(V a[], int len, int o = 1) {  // o=-1 UFWT
+		for (int k = 0; 1 << k < len; ++k)
+			rep(i, 0, len) if (~i >> k & 1) {
+			int j = i ^ (1 << k);
+			(o == 1) ? a[j] += a[i] : a[j] -= a[i];
+		}
+	}
+	int get(V a[], int len, int s) {
+	    int ret=0,k=__builtin_popcount(s);
+	    rep(t,0,s+1) if ((t&s)==t)
+	        if (__builtin_parity(s^t)) ret=add(ret,P-a[t][k]); else ret=add(ret,a[t][k]);
+        return ret;
+    }
+	void pow(V a[], int len, int k, V c[], int ret[]) {
+		fwt(a, len),ret[0]=0;
+		rep(i, 0, len) c[i] = a[i];
+		ret[1]=get(c,len,len-1);
+		rep(j, 2, k + 1) {
+		    rep(i,0,len) c[i] = c[i] * a[i];
+		    ret[j]=get(c,len,len-1);
+        }
+	}
+	void In(int A[], int len, V a[]) {
+		rep(i, 0, len) a[i].clear(), a[i][__builtin_popcount(i)] = A[i];
+	}
+	void Pow(int A[], int len, int k, int C[]) {
+		In(A, len, aa), pow(aa, len, k, bb, C);
+	}
+	void ModP(int a[], int len) {
+		rep(i, 0, len) a[i] = add(a[i], P);
+	}
+};
+
+Calculator<Num<M + 1>> T;
+Poly<int> PP; int X[30],Y[30];
+void Solve(int a[],int n,int mask[],int color[],int ret[]) {
+    mask[0]=1; int L=1<<M;
+    rep(s,1,L) {
+        int t=s&-s,k=__builtin_ctz(t);
+        mask[s]=(mask[s^t] && !(s&a[k]));
+    }
+    T.Pow(mask,L,n,color);
+    rep(i,0,n+1) X[i]=i,Y[i]=color[i];
+    PP.solve(n,X,Y,ret);
+}
+
+/*
+
+Graph: link 
+5 6
+0 2
+1 3
+0 4
+1 4
+2 4
+3 4
+
+Color Ways
+{ 0, 0, 0, 12, 144, 720 } 
+
+Chromatic Poly
+x (x - 1)^2 (x - 2)^2 = {0, 4, -12, 13, -6, 1}
+
+
+Graph: link 
+6 9
+0 1
+1 2
+0 3
+1 3
+2 3
+2 4
+3 4
+0 5
+3 5
+
+Color Ways
+{ 0, 0, 0, 6, 192, 1620, 7680 } 
+
+Chromatic Poly
+{0, -16, 48, -56, 32, -9, 1}
+
+*/
