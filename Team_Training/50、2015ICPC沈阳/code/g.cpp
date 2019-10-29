6493736286f9a2ad4725b27741b9e0b6da99ef8b
fblogy <39649744+fblogy@users.noreply.github.com>
2019-10-29 16:05:06 +0800

    Update g.cpp

--- Team_Training/50、2015ICPC沈阳/code/g.cpp
+++ Team_Training/50、2015ICPC沈阳/code/g.cpp
@@ -13,23 +13,23 @@ using namespace std;
 #define endl '\n'
 #define pw(x) (1ll << (x))
 typedef long long ll;
-typedef double db;
+typedef long double db;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
-const  db eps = 1e-8;
+const db eps = 1e-12;
 
 db w[10], w2[10];
-int T;db v1, v2, t;
+int T;
+db v1, v2, t;
 int sign(db x) { return  (x > eps) - (x < -eps); }
 bool check(db x) {return sign(x) >= 0 && sign(x - 300) <= 0; }
 
-bool solve(db v1,db v2, db t) {
+bool solve(db v1, db v2, db t) {
 	if (sign(v1 - v2) >= 0) return 1;
 	if (sign(v1) == 0) return 0;
-	db sq2 = sqrt(2);
 	int cnt = 0;
-	if (sign(300 * sq2 / v1 - (db) 600 / v2) <= 0) {
+	if (sign(300 * sqrt(2) / v1 - 600 / v2) < 0) {
 		db cc = 1.0 * v1 / v2; cc = cc * cc;
 		db a = 1 - cc, b = -600 * cc, c = 300 * 300 * (1 - cc);
 		db d = sqrt(b * b - 4 * a * c);
@@ -37,28 +37,27 @@ bool solve(db v1,db v2, db t) {
 		db x = 1e18;
 		if (check(xx1)) x = min(x, xx1);
 		if (check(xx2)) x = min(x, xx2);
-		w[0] = x, w2[1] = 300 - x; //else w[0] = xx2, w2[1] = 300 - xx2;
-		w[1] = w[2] = w[3] = 300;
-		w2[0] = 0;  w2[2] = w2[3] = 300;
+		if (sign(a * x * x + b * x + c) != 0) for (;;);
+		w[0] = x; w[1] = w[2] = w[3] = 300;
+		w2[0] = 0; w2[1] = 300 - x; w2[2] = w2[3] = 300;
 		rep(i, 1, 4) {
-			w2[i] += w2[i-1];
 			w[i] += w[i-1];
+			w2[i] += w2[i-1];
 			if (sign(w[i] / v1 - w2[i] / v2 - t) <= 0) cnt++;
 		}
 		cnt++;
 		return cnt >= 3;
-	}else if (300 * v2 <= 900 * v1) {
+	}else if (sign(v2 - 3 * v1) < 0) {
 		db cc = 1.0 * v1 / v2; cc = cc * cc;
 		db a = 1 - cc, b = -600 * (1 + 2 * cc), c = 180000 * (1 - 2 * cc);
 		db d = sqrt(b * b - 4 * a * c);
 		db xx1 = (-b + d) / 2 / a, xx2 = (-b - d) / 2 / a;
 		db x = 1e18;
-		if (check(xx1)) { x = min(x, xx1);}
-		if (check(xx2)) { x = min(x, xx2);}
-		w[0] = sqrt(x * x + 300 * 300); 
-		w2[2] = 300 - x;
-		w[1] = w[2] = w[3] = 300;
-		w2[0] = w2[1] = 0, w2[3] = 300;
+		if (check(xx1)) x = min(x, xx1);
+		if (check(xx2)) x = min(x, xx2);
+		if (sign(a * x * x + b * x + c) != 0) for (;;);
+		w[0] = sqrt(x * x + 300 * 300); w[1] = w[2] = w[3] = 300;
+		w2[0] = w2[1] = 0; w2[2] = 300 - x; w2[3] = 300;
 		rep(i, 1, 4) {
 			w[i] += w[i-1];
 			w2[i] += w2[i-1];
@@ -74,7 +73,7 @@ int main() {
 	std::cin.tie(0);
 	cin >> T;
 	rep(cas, 0, T) {
-		cin >> v1 >> v2 >> t;
+		cin >> t >> v1 >> v2;
 		cout << "Case #" << cas + 1 << ": ";
 		bool ok = solve(v1, v2, t);
 		if (ok) cout << "Yes" << endl;
