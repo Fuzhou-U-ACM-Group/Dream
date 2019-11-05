a735ce9af1443a003a8d56e6daed5b19aff997fa
wowkiee <2240134402@qq.com>
2019-11-05 19:36:12 +0800

    字符串哈希

--- Template/String/StrHash.cpp
+++ Template/String/StrHash.cpp
@@ -1,14 +1,19 @@
-struct STR {
-	ll _B[N], h[N], base, P;
-	inline void init_h(char st[], int n, ll _base = 163, ll _P = 1e9 + 7, char c = 'a') {
-		base = _base, P = _P, h[0] = st[0] - c;
-		rep(i, 1, n) h[i] = (h[i - 1] * base + st[i] - c) % P;
-		if (_B[0] == 1) return; _B[0] = 1;
-		rep(i, 1, N) _B[i] = _B[i - 1] * base %P;
+const int P = 1e9 + 7, N = 101010;
+struct Str {
+	int B[N], h[N], ba;
+	Str(int ba) : ba(ba) { 
+		B[0] = 1; rep(i, 1, N) B[i] = mul(B[i - 1], ba);
 	}
-	inline ll H(int l, int r) {
-		if (!l) return h[r] < 0 ? h[r] + P : h[r];
-		ll ans = (h[r] - h[l - 1] * _B[r - l + 1]) % P;
-		return ans < 0 ? ans + P : ans;
+	int upd(int a, int b) {
+		if((a += b) >= P) a -= P;
+		return a < 0 ? a + P : a;
 	}
-};
+	int mul(int a, int b) { return 1ll * a * b % P; }
+	void init(vi &s) {
+		h[0] = s[0] + 1; rep(i, 1, sz(s)) h[i] = upd(mul(h[i - 1], ba), s[i] + 1);
+	}
+	int sub(int l, int r) {
+		if(!l) return h[r];
+		return upd(h[r], -mul(h[l - 1], B[r - l + 1]));
+	}
+}ha1(233), ha2(241);
