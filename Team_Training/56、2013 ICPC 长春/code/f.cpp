24e8521ceb068e577e3226b029bf136a0e2158c0
fblogy <39649744+fblogy@users.noreply.github.com>
2019-11-05 21:27:37 +0800

    Update f.cpp

--- Team_Training/56、2013 ICPC 长春/code/f.cpp
+++ Team_Training/56、2013 ICPC 长春/code/f.cpp
@@ -17,34 +17,25 @@ typedef pair<int, int> pii;
 typedef long double db;
 typedef vector<int> vi;
 
-const int N = 111;
+const int N = 211;
 
 db a[N][N];
 db x[N], sx[N];
+
+vi g[N], ng[N];
+int n, m;
+bool vis[N];
+db px;
+
 
 namespace GaussDB {
 	static const int N = 505;
 	int free[N], fnum, k, col, p;
 	const db eps = 1e-14;
 
-	void genx(int var) {
-		int pre = var; fnum = 0;
-		per(i, 0, k) {
-			rep(j, 0, var) if(fabs(a[i][j]) > eps) { p = j; break; }
-			rep(j, 0, i) if(fabs(a[j][p]) > eps) {
-				db t = a[j][p] / a[i][p];
-				rep(l, p, var + 1) a[j][l] -= a[i][l] * t;
-			}
-			rep(j, p + 1, pre) free[fnum++] = j; // x[j] = ?;
-			pre = p;
-			x[p] = a[i][var];
-			rep(j, p + 1, var) x[p] -= a[i][j] * x[j];
-			x[p] /= a[i][p];
-		}
-		rep(j, 0, pre) free[fnum++] = j;
-	}
 	int Gauss(int equ, int var) {
-		for(k = col = 0; k < equ && col < var; ++k, ++col) {
+		//rep(i, 0, equ) rep(j, 0, var+1) cout << a[i][j] << " \n"[j == var];
+		for(k = col = 0; k < equ && col < n; ++k, ++col) {
 			p = k; rep(i, k + 1, equ) if(fabs(a[i][col]) > fabs(a[p][col])) p = i;
 			if(p != k) rep(j, col, var + 1) swap(a[p][j], a[k][j]);
 			if(fabs(a[k][col]) < eps) { k--; continue; }
@@ -55,23 +46,18 @@ namespace GaussDB {
 			}
 		}
 		rep(i, k, equ) if(fabs(a[i][var]) > eps) return -1;
-		if(k < var) {
-			return var - k;
-		}
-		per(i, 0, var) {
+		if(k < n) return 1;
+		rep(i, 0, n - 1) if (!vis[i]) x[i] = a[n-1][var] / a[n-1][i + n];
+		x[n-1] = a[n-1][var] / a[n-1][n-1];
+		/*per(i, 0, var) {
 			db t = a[i][var];
 			rep(j, i + 1, var) if(fabs(a[i][j]) > eps) t -= x[j] * a[i][j];
 			x[i] = t / a[i][i];
-		}
+		}*/
 		return 0;
 	}
 };
 
-vi g[N], ng[N];
-int n, m;
-bool vis[N];
-db y[N], tmp[N];
-db px;
 
 const db eps = 1e-8;
 
@@ -79,40 +65,11 @@ int sign(db x) {
 	return (x > eps) - (x < -eps);
 }
 
-
-bool check1() {
-	rep(i, 0, n) y[i] = sx[i];
-	rep(t, 0, 10) {
-		rep(i, 0, n) {
-			db c = y[i] / sz(g[i]);
-			for(auto j : g[i]) tmp[j] += c;
-		}
-		rep(j, 0, n) y[j] = tmp[j], tmp[j] = 0;
-	}
-	return sign(y[n - 1] - sx[n - 1]) > 0;
-}
-
-bool check() {
-	rep(i, 0, n + 1) rep(j, 0, n + 1) a[i][j] = 0;
-	rep(j, 0, n + 1) a[n][j] = 1;
-	rep(i, 0, n) {
-		for(auto j : ng[i]) a[i][j] = 1. / sz(g[j]);
-		a[i][i] = -1;
-	}
-	int res = GaussDB::Gauss(n + 1, n);
-	if(res == 0 && sign(x[n - 1] - px) > 0) return 1;
-	return 0;
-}
-
 int work() {
 	int res = -1;
 	px = x[n - 1];
 	rep(i, 0, n - 1) if(!vis[i]) {
-		g[n - 1].pb(i);
-		ng[i].pb(n - 1);
-		if(check1() && check()) px = x[n - 1], res = i;
-		g[n - 1].pop_back();
-		ng[i].pop_back();
+		if (sign(x[i] - px) > 0) px = x[i], res = i;
 	}
 	return res;
 }
@@ -126,14 +83,19 @@ void solve() {
 		ng[v].pb(u);
 		if(u == n - 1) vis[v] = 1;
 	}
-	rep(i, 0, n + 1) rep(j, 0, n + 1) a[i][j] = 0;
-	rep(j, 0, n + 1) a[n][j] = 1;
+	rep(i, 0, n + 1) rep(j, 0, n + n + 1) a[i][j] = 0;
+	rep(j, 0, n + n) a[n][j] = 1;
 	rep(i, 0, n) {
 		for(auto j : ng[i]) a[i][j] = 1. / sz(g[j]);
 		a[i][i] = -1;
 	}
-	int res = GaussDB::Gauss(n + 1, n);
-	rep(i, 0, n) sx[i] = x[i];
+	rep(i, 0, n-1) if (!vis[i]) {
+		for (auto j : g[n - 1]) a[j][n + i] = 1.0 / (sz(g[n-1]) + 1);
+		a[i][n + i] = 1.0 / (sz(g[n-1]) + 1);  
+		a[n-1][n + i] = -1;
+	}
+	
+	int res = GaussDB::Gauss(n + 1, n + n - 1);
 	if(res != 0) {
 		cout << "INF" << endl;
 		return ;
@@ -143,6 +105,7 @@ void solve() {
 }
 
 int main() {
+	freopen("a.in", "r", stdin);
 	std::ios::sync_with_stdio(0);
 	std::cin.tie(0);
 	int T; cin >> T;
